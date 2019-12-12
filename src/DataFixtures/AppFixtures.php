77bafcad1cfa2dfcd47134275ed6f0b0ebc2c586
pierre gosselin <pierre.gosselin@orange.fr>
2019-12-12 13:56:21 +0100

    'Entity_book'

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Books;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -12,6 +13,25 @@ class AppFixtures extends Fixture
         // $product = new Product();
         // $manager->persist($product);
 
+        $book = new Books();
+        $book->setTitle("Samuel à la plage");
+        $book->setPrice("45");
+        $manager->persist($book);
+
+        $book = new Books();
+        $book->setTitle("Samuel apprend le code");
+        $book->setPrice("45");
+        $manager->persist($book);
+
+        $book = new Books();
+        $book->setTitle("Samuel sort de sa cité");
+        $book->setPrice("45");
+        $manager->persist($book);
+
+        $book = new Books();
+        $book->setTitle("Samuel veut gagner de l'argent");
+        $book->setPrice("45");
+        $manager->persist($book);
         $manager->flush();
     }
 }
